b23d3388b9af564bea185ee26b2a253a98eaafe9
Nathan Westfall <nathan@fistbumpstudios.com>
2017-11-18 09:13:22 -0500

    Add project files.

---
+++ DigitalOceanUploader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DigitalOceanUploader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

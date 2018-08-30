e78332bc98e84ff0aa74cb16de6ea51919c3836a
Nathan Westfall <nathan@fistbumpstudios.com>
2018-08-30 10:16:46 -0400

    support having a shared library so a DLL can be used with public APIs

--- DigitalOceanUploader/Program.cs
+++ DigitalOceanUploader/Program.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.IO;
 using System.Linq;
-using System.Security;
 
 using HeyRed.Mime;
 using Amazon.S3;
 using Amazon.Runtime;
+using DigitalOceanUploader.Shared;
 
 namespace DigitalOceanSpacesManager
 {
@@ -29,42 +29,42 @@ namespace DigitalOceanSpacesManager
             #region Keys
             Console.Write("Enter DO Access Key: ");
             ConsoleKeyInfo key;
-            KeyManager.ACCESS_KEY.Clear();
+			KeyManager keyManager = new KeyManager();
             do
             {
                 key = Console.ReadKey(true);
 
                 if (key.Key != ConsoleKey.Backspace && key.Key != ConsoleKey.Enter)
                 {
-                    KeyManager.ACCESS_KEY.AppendChar(key.KeyChar);
+                    keyManager.AccessKey.AppendChar(key.KeyChar);
                     Console.Write("*");
                 }
                 else
                 {
-                    if (key.Key == ConsoleKey.Backspace && KeyManager.ACCESS_KEY.Length > 0)
+                    if (key.Key == ConsoleKey.Backspace && keyManager.AccessKey.Length > 0)
                     {
-                        KeyManager.ACCESS_KEY.RemoveAt(KeyManager.ACCESS_KEY.Length - 1);
+                        keyManager.AccessKey.RemoveAt(keyManager.AccessKey.Length - 1);
                         Console.Write("\b \b");
                     }
                 }
             } while (key.Key != ConsoleKey.Enter);
             Console.Write("\n");
             Console.Write("Enter DO Secrey Key: ");
-            KeyManager.SECRET_KEY.Clear();
+            keyManager.SecretKey.Clear();
             do
             {
                 key = Console.ReadKey(true);
 
                 if (key.Key != ConsoleKey.Backspace && key.Key != ConsoleKey.Enter)
                 {
-                    KeyManager.SECRET_KEY.AppendChar(key.KeyChar);
+					keyManager.SecretKey.AppendChar(key.KeyChar);
                     Console.Write("*");
                 }
                 else
                 {
-                    if (key.Key == ConsoleKey.Backspace && KeyManager.ACCESS_KEY.Length > 0)
+                    if (key.Key == ConsoleKey.Backspace && keyManager.SecretKey.Length > 0)
                     {
-                        KeyManager.ACCESS_KEY.RemoveAt(KeyManager.ACCESS_KEY.Length - 1);
+						keyManager.SecretKey.RemoveAt(keyManager.SecretKey.Length - 1);
                         Console.Write("\b \b");
                     }
                 }
@@ -72,15 +72,13 @@ namespace DigitalOceanSpacesManager
             Console.Write("\n");
             #endregion
 
-            var client = new AmazonS3Client(KeyManager.SecureStringToString(KeyManager.ACCESS_KEY), KeyManager.SecureStringToString(KeyManager.SECRET_KEY), new AmazonS3Config()
-            {
-                ServiceURL = "https://nyc3.digitaloceanspaces.com"
-            });
-            client.ExceptionEvent += Client_ExceptionEvent;
-
             string filePath, uploadName, spaceName, contentType = string.Empty;
             Console.Write("Enter Space name to use: ");
             spaceName = Console.ReadLine();
+
+			// Can now setup manager
+			DigitalOceanUploadManager digitalOceanUploadManager = new DigitalOceanUploadManager(keyManager, spaceName);
+
             bool fileExists = false;
             do
             {
@@ -103,98 +101,34 @@ namespace DigitalOceanSpacesManager
             var wipeAway = Console.ReadLine();
             if(wipeAway == "Y")
             {
-                var currentMultiParts = await client.ListMultipartUploadsAsync(spaceName);
-                foreach (var multiPart in currentMultiParts.MultipartUploads)
-                {
-                    try
-                    {
-                        await client.AbortMultipartUploadAsync(currentMultiParts.BucketName, multiPart.Key, multiPart.UploadId);
-                    }
-                    catch(Exception) { }
-                }
-
-                Console.WriteLine("Wiped away previous upload attempts");
+				await digitalOceanUploadManager.CleanUpPreviousAttempts();
             }
 
-            var fileInfo = new FileInfo(filePath);
+			digitalOceanUploadManager.UploadStatusEvent += DigitalOceanUploadManager_UploadStatusEvent;
+			digitalOceanUploadManager.UploadExceptionEvent += DigitalOceanUploadManager_UploadExceptionEvent;
+			var uploadId = await digitalOceanUploadManager.UploadFile(filePath, uploadName);
+			Console.WriteLine("File upload complete");
+			digitalOceanUploadManager.UploadStatusEvent -= DigitalOceanUploadManager_UploadStatusEvent;
+			digitalOceanUploadManager.UploadExceptionEvent -= DigitalOceanUploadManager_UploadExceptionEvent;
 
-            var multiPartStart = await client.InitiateMultipartUploadAsync(new Amazon.S3.Model.InitiateMultipartUploadRequest()
-            {
-                BucketName = spaceName,
-                ContentType = contentType,
-                Key = uploadName
-            });
-            try
-            {
-                var i = 0L;
-                var n = 1;
-                Dictionary<string, int> parts = new Dictionary<string, int>();
-                while (i < fileInfo.Length)
-                {
-                    Console.WriteLine($"Starting upload for Part #{n}");
-                    long partSize = 6000000;
-                    var lastPart = (i + partSize) >= fileInfo.Length;
-                    if (lastPart)
-                        partSize = fileInfo.Length - i;
-                    bool complete = false;
-                    int retry = 0;
-                    Amazon.S3.Model.UploadPartResponse partResp = null;
-                    do
-                    {
-                        retry++;
-                        try
-                        {
-                            partResp = await client.UploadPartAsync(new Amazon.S3.Model.UploadPartRequest()
-                            {
-                                BucketName = spaceName,
-                                FilePath = filePath,
-                                FilePosition = i,
-                                IsLastPart = lastPart,
-                                PartSize = partSize,
-                                PartNumber = n,
-                                UploadId = multiPartStart.UploadId,
-                                Key = uploadName
-                            });
-                            complete = true;
-                        }
-                        catch(Exception)
-                        {
-                            Console.WriteLine($"Failed to upload part {n} on try #{retry}...");
-                        }
-                    } while (!complete && retry <= 3);
+			digitalOceanUploadManager?.Dispose();
+			digitalOceanUploadManager = null;
 
-                    if (!complete || partResp == null)
-                        throw new Exception($"Unable to upload part {n}... Failing");
+			keyManager?.Dispose();
+			keyManager = null;
 
-                    parts.Add(partResp.ETag, n);
-                    i += partSize;
-                    n++;
-                    Console.WriteLine($"Uploading {(((float)i/(float)fileInfo.Length) * 100).ToString("N2")} ({i}/{fileInfo.Length})");
-                }
-
-                Console.WriteLine("Done uploading!  Completing upload");
-                var completePart = await client.CompleteMultipartUploadAsync(new Amazon.S3.Model.CompleteMultipartUploadRequest()
-                {
-                    UploadId = multiPartStart.UploadId,
-                    BucketName = spaceName,
-                    Key = uploadName,
-                    PartETags = parts.Select(p => new Amazon.S3.Model.PartETag(p.Value, p.Key)).ToList()
-                });
-                Console.WriteLine("Successfully uploaded!");
-            }
-            catch(Exception ex)
-            {
-                var abortPart = await client.AbortMultipartUploadAsync(spaceName, uploadName, multiPartStart.UploadId);
-                Console.WriteLine("Error while uploading! " + ex.Message);
-                await Task.Delay(10000);
-            }
             Console.WriteLine("Press enter to exit");
             Console.ReadLine();
         }
 
-        private static void Client_ExceptionEvent(object sender, ExceptionEventArgs e)
-        {
-            Console.WriteLine($"Error with S3 Client occurred: {e}");
-        }
+		static void DigitalOceanUploadManager_UploadStatusEvent(object sender, UploadStatus e)
+		{
+			Console.WriteLine($"File Upload Status: Part {e.PartNumber}/{e.EstimatedParts} ({e.BytesUploaded}/{e.TotalBytes})");
+		}
+
+		static void DigitalOceanUploadManager_UploadExceptionEvent(object sender, UploadException e)
+		{
+			Console.WriteLine(e.Message);
+		}
     }
 }
